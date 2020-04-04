d83206bbd89c1fedf45a4ea8003aaad230c9fa90
uladzislau.kaminski <uladzislau.kaminski@nordstrom.com>
2020-04-05 01:22:02 +0300

    Path compression

--- src/com/uladkaminski/unionfind/BalancedQuickUnion.java
+++ src/com/uladkaminski/unionfind/BalancedQuickUnion.java
@@ -33,6 +33,7 @@ public class BalancedQuickUnion implements UnionFind {
 
     private int root(int i) {
         while (arr[i] != i) {
+            arr[i] = arr[arr[i]];
             i = arr[i];
         }
 
